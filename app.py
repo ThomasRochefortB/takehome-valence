24544a3e135cb1813ad6d29b9ae286f7617f2247
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2024-06-09 14:05:06 -0400

    Added starters on the app

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from langchain_cohere import ChatCohere
 from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 from langchain.agents import AgentExecutor
 from langchain_cohere.react_multi_hop.agent import create_cohere_react_agent
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.memory import ChatMessageHistory,ConversationBufferMemory
+from langchain.memory import ChatMessageHistory
 from tools import query_vector_db_articles, get_smiles_from_pubchem, predict_energy_from_smiles
 
 import chainlit as cl
 
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="What disease are they discussing in the paper 'Targeted therapy in advanced non-small cell lung cancer: current advances and future trends.'?",
+            message="What disease are they discussing in the paper 'Targeted therapy in advanced non-small cell lung cancer: current advances and future trends.'? ",
+            icon="/public/PDF_icon.svg",
+            ),
+
+        cl.Starter(
+            label="Find SMILES of Molecule",
+            message="Can you find the SMILES string of Ibuprofen?",
+            icon="/public/Caffeine.svg",
+            ),
+        cl.Starter(
+            label="Predict Hydration Free Energy",
+            message="Can you predict the energy value of 'CCO'? ",
+            icon="/public/lightning.svg",
+            ),
+
+        ]
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -37,12 +55,10 @@ async def on_chat_start():
                                 verbose=True,
                                 handle_parsing_errors=True,)
     
-    memory = ChatMessageHistory()#session_id="test-session")
+    memory = ChatMessageHistory()
 
     runnable = RunnableWithMessageHistory(
         agent_executor,
-        # This is needed because in most real world scenarios, a session id is needed
-        # It isn't really used here because we are using a simple in memory ChatMessageHistory
         lambda session_id: memory,
         memory=memory,
         input_messages_key="input",
